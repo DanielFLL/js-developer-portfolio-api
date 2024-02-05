54feece86feed5987778573b0921a474552d0eb7
DanielFLL <daniel-dfl@hotmail.com>
2024-02-05 11:58:13 -0300

    commit final

--- assets/js/acordeon.js
+++
@@ -1,8 +0,0 @@
-const acordeonTriggers = document.querySelectorAll('.acordeon .trigger');
-
-acordeonTriggers.forEach((trigger) => {
-    trigger.addEventListener('click', () => {
-        const acordeon = trigger.parentElement; 
-        acordeon.classList.toggle('open');
-    });
-});
--- assets/js/api.js
+++
@@ -1,5 +0,0 @@
-async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/DanielFLL/js-developer-portfolio/main/data/profile.json';
-    const fetching = await fetch(url);
-    return await fetching.json();
-};
