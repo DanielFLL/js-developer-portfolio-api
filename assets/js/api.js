b9c5b9793b19db80c7f1059b05df707a4e21c803
DanielFLL <daniel-dfl@hotmail.com>
2024-02-05 14:37:04 -0300

    correção profile na parte education

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,5 @@
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/DanielFLL/js-developer-portfolio/main/data/profile.json';
+    const url = 'https://raw.githubusercontent.com/DanielFLL/js-developer-portfolio-api/main/data/profile.json';
     const fetching = await fetch(url);
     return await fetching.json();
 };
